fa26152b4d6e5f8260ec039579be7cd468492e6a
parthi <Stackholic-Infotech@dev.azure.com>
2023-05-18 17:24:02 +0530

    navbar change

--- src/Components/Navbar/Navbar.js
+++ src/Components/Navbar/Navbar.js
@@ -1,30 +1,31 @@
 import React from "react";
 import { Link, NavLink } from "react-router-dom";
 import AllToolsList from "./AllTools/AllToolsList";
-import ConvertList from "./CovertList/ConvertList";
-import HumBurgerList from "./HumburgerList/HumBurgerList";
+// import HumBurgerList from "./HumburgerList/HumBurgerList";
 
 import Logo from "../../Assets/image/Logo.png";
 
 import "./Navbar.css";
+import { Container } from "@mui/material";
 
 const Navbar = () => {
   return (
     <>
       {/* Navbar */}
       <div className="header">
-        <div className="header__main">
-          {/* logo  */}
-          <div className="brand">
-            <div className="brand__logo">
-              <Link to="/" title="PDFLover">
-                <img src={Logo} alt="PDFLover" />
-              </Link>
+        <Container>
+          <div className="header__main">
+            {/* logo  */}
+            <div className="brand">
+              <div className="brand__logo">
+                <Link to="/" title="PDFLover">
+                  <img src={Logo} alt="PDFLover" />
+                </Link>
+              </div>
             </div>
-          </div>
 
-          {/* Navbar left data  */}
-          <div className="header__nav">
+            {/* Navbar left data  */}
+            {/* <div className="header__nav">
             <div className="main-menu" id="menuBig">
               <ul className="nav">
                 <li>
@@ -55,25 +56,6 @@ const Navbar = () => {
                   </NavLink>
                 </li>
 
-                <li className="dropdown dropdown-full">
-                  <NavLink
-                  // className={({ isActive }) =>
-                  //   isActive ? "nav__item--active" : undefined
-                  // }
-                  // to="/jpg_to_pdf"
-                  // end
-                  >
-                    Convert PDF{" "}
-                    <i
-                      className="fa-solid fa-caret-down"
-                      style={{ marginLeft: "10px" }}
-                    />
-                  </NavLink>
-                  <ul className="dropdown-menu mega menu-full menu-full--convert">
-                    <ConvertList />
-                  </ul>
-                </li>
-
                 <li className="dropdown dropdown-full" id="menuSmall">
                   <span>
                     All PDF tools{" "}
@@ -95,19 +77,19 @@ const Navbar = () => {
                 </li>
               </ul>
             </div>
-          </div>
+          </div> */}
 
-          {/* Navbar Right Data */}
-          <div
+            {/* Navbar Right Data */}
+            {/* <div
             className="desktop tooltip tooltip--bottom"
             title="PDFLover Desktop, work offline">
             <Link to="" title="Desktop">
               <i className="fa-solid fa-desktop" />
             </Link>
-          </div>
+          </div> */}
 
-          <div className="top-menu">
-            <ul className="nav">
+            <div className="top-menu">
+              {/* <ul className="nav">
               <li className="nav__item nav__item--gray">
                 <Link to="/Login">
                   <span>Log in</span>
@@ -125,27 +107,82 @@ const Navbar = () => {
                 <i className="fa-solid fa-bars-staggered" />
                 <HumBurgerList />
               </li>
-            </ul>
+            </ul> */}
 
-            <div className="header__nav responsive__nav">
-              <div className="main-menu" id="menuBig">
-                <ul className="nav">
-                  <li className="dropdown dropdown-full" id="menuSmall">
-                    <span
-                      className="nav__icon__res"
-                      data-toggle="dropdown">
-                      <i className="fa-solid fa-bars-staggered" />
-                    </span>
+              <div className="header__nav">
+                <div className="main-menu" id="menuBig">
+                  <ul className="nav">
+                    <li>
+                      <NavLink
+                        className={({ isActive }) =>
+                          isActive ? "nav__item--active" : undefined
+                        }
+                        to="/Merge_PDF">
+                        Merge PDF
+                      </NavLink>
+                    </li>
+                    <li>
+                      <NavLink
+                        className={({ isActive }) =>
+                          isActive ? "nav__item--active" : undefined
+                        }
+                        to="/Split_PDF">
+                        Split PDF
+                      </NavLink>
+                    </li>
+                    <li>
+                      <NavLink
+                        className={({ isActive }) =>
+                          isActive ? "nav__item--active" : undefined
+                        }
+                        to="/Compress_PDF">
+                        Compress PDF
+                      </NavLink>
+                    </li>
 
-                    <ul className="dropdown-menu mega menu-full" style={{ height: "35em" }}>
-                      <AllToolsList />
-                    </ul>
-                  </li>
-                </ul>
+                    <li className="dropdown dropdown-full" id="menuSmall">
+                      <span>
+                        All PDF tools{" "}
+                        <i
+                          className="fa-solid fa-caret-down"
+                          style={{ marginLeft: "10px" }}
+                        />
+                      </span>
+
+                      <span
+                        className="nav__icon dropdown-toggle"
+                        data-toggle="dropdown">
+                        <i className="fa-solid fa-wrench" />
+                      </span>
+
+                      <ul className="dropdown-menu mega menu-full" style={{ height: "35em" }}>
+                        <AllToolsList />
+                      </ul>
+                    </li>
+                  </ul>
+                </div>
+              </div>
+
+              <div className="header__nav responsive__nav">
+                <div className="main-menu" id="menuBig">
+                  <ul className="nav">
+                    <li className="dropdown dropdown-full" id="menuSmall">
+                      <span
+                        className="nav__icon__res"
+                        data-toggle="dropdown">
+                        <i className="fa-solid fa-bars-staggered" />
+                      </span>
+
+                      <ul className="dropdown-menu mega menu-full" style={{ height: "35em" }}>
+                        <AllToolsList />
+                      </ul>
+                    </li>
+                  </ul>
+                </div>
               </div>
             </div>
           </div>
-        </div>
+        </Container>
       </div>
     </>
   );
