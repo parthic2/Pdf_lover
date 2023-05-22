6f60ad454e8b4037fd381f11fe7185ba9b1f619f
parthi <Stackholic-Infotech@dev.azure.com>
2023-05-22 09:57:53 +0530

    c

--- src/Components/Main/Index.js
+++ src/Components/Main/Index.js
@@ -1,7 +1,7 @@
 import React from "react";
 import PreSection from "../PreSection/PreSection";
 import Banner from "./Banner/Banner";
-import Main from "./Main";
+// import Main from "./Main";
 import { useEffect } from "react";
 
 const Index = () => {
