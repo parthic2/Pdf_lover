0ffb565d28daf5588b3df9a233378ebe12668a5d
parthi <Stackholic-Infotech@dev.azure.com>
2023-08-28 09:53:38 +0530

    28-08-23

--- src/Components/SkeletonLoader/SkeletonLoader.jsx
+++ src/Components/SkeletonLoader/SkeletonLoader.jsx
@@ -4,42 +4,42 @@ import { Box } from "@mui/material";
 import style from "../../Pages/Pages.module.css";
 
 const SkeletonLoader = () => {
-    return (
-        <div className={style.main}>
-            <div className={style.tool}>
-                <div className={style.tool__workarea} id="workArea">
-                    <div className={style.tool__header}>
-                        <div className={style["skeleton-container"]}>
-                            <Box
-                                className={style["skeleton-box"]}
-                                sx={{
-                                    width: "30%",
-                                }}
-                            >
-                                <Skeleton height={150} width={150} />
-                            </Box>
-                            <Box
-                                className={style["skeleton-box"]}
-                                sx={{
-                                    width: "60%",
-                                }}
-                            >
-                                <Skeleton height={150} width={150} />
-                            </Box>
-                            <Box
-                                className={style["skeleton-box"]}
-                                sx={{
-                                    width: "20%",
-                                }}
-                            >
-                                <Skeleton height={150} width={150} />
-                            </Box>
-                        </div>
-                    </div>
-                </div>
+  return (
+    <div className={style.main}>
+      <div className={style.tool}>
+        <div className={style.tool__workarea} id="workArea">
+          <div className={style.tool__header}>
+            <div className={style["skeleton-container"]}>
+              <Box
+                className={style["skeleton-box"]}
+                sx={{
+                  width: "30%",
+                }}
+              >
+                <Skeleton height={150} width={150} />
+              </Box>
+              <Box
+                className={style["skeleton-box"]}
+                sx={{
+                  width: "60%",
+                }}
+              >
+                <Skeleton height={150} width={150} />
+              </Box>
+              <Box
+                className={style["skeleton-box"]}
+                sx={{
+                  width: "20%",
+                }}
+              >
+                <Skeleton height={150} width={150} />
+              </Box>
             </div>
+          </div>
         </div>
-    )
+      </div>
+    </div>
+  )
 }
 
 export default SkeletonLoader;
